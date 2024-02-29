6346b6c28220659190b872d9ddb9bc856888b1fd
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-02-29 12:10:48 -0300

    :sparkles: implemented update modal
    Furthermore, a button to open the modal was added

--- src/app/[admin]/answers/components/AnswerCard.tsx
+++ src/app/[admin]/answers/components/AnswerCard.tsx
@@ -2,11 +2,18 @@
 // Types
 import { IAnswer } from "@/app/common/types";
 // Components
-import { Button, Card, CardBody, Image } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Image,
+  useDisclosure,
+} from "@nextui-org/react";
+import UpdateAnswerModal from "./UpdateAnswerModal";
 // Stories
 import { useAnswerStore } from "../store";
 //Icons
-import { RemoveIcon } from "@/app/common/icons";
+import { RemoveIcon, EditIcon } from "@/app/common/icons";
 
 interface Props {
   answer: Partial<IAnswer>;
@@ -15,6 +22,7 @@ interface Props {
 const AnswerCard = ({ answer }: Props) => {
   const { deleteAnswer, isDeletingAnswer, fetchAll } = useAnswerStore();
   const { description, imageUrl, uid, statement } = answer;
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const handleDelete = async () => {
     if (!uid) return;
@@ -23,24 +31,41 @@ const AnswerCard = ({ answer }: Props) => {
   };
 
   return (
-    <Card className="w-full hover:ring-1 hover:scale-[101%]">
-      <CardBody className="flex flex-row items-center justify-between">
-        <div className="items-center flex gap-8">
-          <Image className="w-20 h-16" alt={description} src={imageUrl} />
-          <h4>{statement}</h4>
-        </div>
-        <div className="flex items-center gap-2">
-          <Button
-            className="bg-red text-white"
-            onClick={handleDelete}
-            isLoading={isDeletingAnswer}
-            startContent={<RemoveIcon />}
-          >
-            Deletar
-          </Button>
-        </div>
-      </CardBody>
-    </Card>
+    <>
+      <Card className="w-full hover:ring-1 hover:scale-[101%]">
+        <CardBody className="flex flex-row items-center justify-between">
+          <div className="items-center flex gap-8">
+            <Image className="w-20 h-16" alt={description} src={imageUrl} />
+            <h4>{statement}</h4>
+          </div>
+          <div className="flex items-center gap-2">
+            <Button
+              className="bg-green text-white"
+              onClick={onOpen}
+              isLoading={isDeletingAnswer}
+              startContent={<EditIcon />}
+            >
+              Atualizar
+            </Button>
+            <Button
+              className="bg-red text-white"
+              onClick={handleDelete}
+              isLoading={isDeletingAnswer}
+              startContent={<RemoveIcon />}
+            >
+              Deletar
+            </Button>
+          </div>
+        </CardBody>
+      </Card>
+      <UpdateAnswerModal
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+        fetchAll={fetchAll}
+        answer={answer}
+      />
+    </>
   );
 };
 
