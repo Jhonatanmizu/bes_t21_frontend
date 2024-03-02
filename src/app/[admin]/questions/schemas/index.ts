c17dfd16bc881ab7be2dd1b22921aad78608f2f1
jhonatanmizu <natanjesuss20@gmail.com>
2024-03-02 20:33:45 -0300

    :recycle: refactor: add point and correctAnswers to questions schema

--- src/app/[admin]/questions/schemas/index.ts
+++ src/app/[admin]/questions/schemas/index.ts
@@ -1,12 +1,14 @@
-import { QuestionLevel, QuestionType } from "@/app/common/enums";
 import z from "zod";
+// Types
+import { QuestionLevel, QuestionType } from "@/app/common/enums";
 
 const questionSchema = z.object({
   uid: z.coerce.string(),
   description: z.coerce.string(),
   level: z.enum([QuestionLevel.EASY, QuestionLevel.MEDIUM, QuestionLevel.HARD]),
   type: z.enum([QuestionType.INPUT, QuestionType.MULTIPLE, QuestionType.VOICE]),
-  answerUid: z.coerce.string(),
+  point: z.number(),
+  correctAnswers: z.array(z.string()),
 });
 
 export type questionData = z.infer<typeof questionSchema>;
