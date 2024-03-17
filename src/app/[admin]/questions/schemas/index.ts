35c6f68231a8f2669cb1395b09866b97c3ee9345
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-03-17 12:27:33 -0300

    :recycle: refactoring the validation schema

--- src/app/[admin]/questions/schemas/index.ts
+++ src/app/[admin]/questions/schemas/index.ts
@@ -1,23 +1,28 @@
-import z from "zod";
-// Types
+import { z } from "zod";
 import { QuestionLevel, QuestionType } from "@/app/common/enums";
 
 export const questionSchema = z.object({
-  uid: z.coerce.string().nullable().default(null),
-  img: z.coerce.string().nullable().default(null),
-  description: z.coerce.string({ required_error: "Descrição é obrigatória" }),
+  uid: z.string().nullable().default(null),
+  img: z.string().nullable().default(null),
+  description: z
+    .string({ required_error: "Descrição é obrigatória" })
+    .min(1, "Descrição é obrigatória"),
   level: z.enum(
     [QuestionLevel.EASY, QuestionLevel.MEDIUM, QuestionLevel.HARD],
     { required_error: "É necessário informar o nível da questão" }
   ),
   type: z.enum(
-    [QuestionType.INPUT, QuestionType.MULTIPLE, QuestionType.VOICE],
-    { required_error: "É necessário informar o tipo da questão" }
+    [
+      QuestionType.INPUT,
+      QuestionType.MULTIPLE,
+      QuestionType.VOICE,
+      QuestionType.SELECT,
+    ],
+    {
+      required_error: "É necessário informar o tipo da questão",
+      invalid_type_error: "Você deve selecionar o tipo da questão",
+    }
   ),
-  point: z.number().min(1, "A pontuação deve ser maior que zero"),
-  correctAnswers: z
-    .array(z.string())
-    .min(1, "Deve haver ao menos uma questão correta"),
 });
 
 export type questionData = z.infer<typeof questionSchema>;
